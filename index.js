7e456ccf8e1969d2f7b1b5d6cc5ebb5f3e92b840
Matthew <mattj20@gmail.com>
2022-10-08 15:10:16 -0700

    commit tree 944c368535a77f6297f79c5146a504647b6b48f5

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let breakTime = time.split(':');
+  let convertTime = parseInt(breakTime[0]);
+  if (convertTime < 12) {
+    return "Good Morning"
+  } else if (convertTime >= 12 && convertTime < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById('greeting').innerHTML = message
+}
